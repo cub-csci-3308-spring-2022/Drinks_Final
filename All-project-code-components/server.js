11a321ab32f292b3698d62179288156f41570ef5
Duncan Miller <dumi3535@colorado.edu>
2022-04-11 18:09:55 -0600

    combined login and signup and sent user info to home.ejs

--- All-project-code-components/server.js
+++ All-project-code-components/server.js
@@ -19,21 +19,21 @@ const dbConfig = {
 
 var db = pgp(dbConfig);
 var selected_ingredients = [];
-
+var currentuser;
 
 app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
 app.set("view engine", "ejs"); // to be able to view the ejs file
 
 
+app.get("/users/home", (req, res) => {
+  res.render("home.ejs",{currentuser:currentuser}); // to view the homepage inex is the name
+});
+
+// app.get("/users/signup", (req, res) => {
+//   res.render("signup.ejs");
+// });
+
 app.get("/", (req, res) => {
-  res.render("home"); // to view the homepage inex is the name
-});
-
-app.get("/users/signup", (req, res) => {
-  res.render("signup.ejs");
-});
-
-app.get("/users/login", (req, res) => {
   res.render("login.ejs"); // to view the login page
 });
 
@@ -84,7 +84,7 @@ app.post("/users/signup", function (req, res) {
           ]);
       })
       .then(info => {
-        res.render("home.ejs")
+        res.render("login.ejs")
       })
       .catch(err => {
               console.log('error', err);
@@ -98,7 +98,7 @@ app.post("/users/login.ejs", function(req, res) {
   var username = req.body.username;
 	var password = req.body.password;
 	//var query = 'select username, password from users;';
-	var query = "select username, password from users where username = '" + username + "';";
+	var query = "select * from users where username = '" + username + "';";
 	db.any(query)
         .then(function (rows) {
 					console.log(rows);
@@ -107,7 +107,9 @@ app.post("/users/login.ejs", function(req, res) {
 					// 	return
 					// }
       		if(rows[0].password == password){
-						return res.render("home");
+						currentuser = rows[0];
+						console.log(currentuser);
+						return res.redirect("/users/home");
 					}
 					else{
 						console.log({
@@ -125,8 +127,10 @@ app.post("/users/login.ejs", function(req, res) {
 });
 
 // Dummy Load Database
-// INSERT INTO users(firstName, lastName, DOB, username, password) VALUES('Duncan','Miller','20000212','duncanmiller','DuncanMiller1!');
+// INSERT INTO users(firstName, lastName, DOB, username, password) VALUES('easy','checkc','20000101','user','pass');
 
+// Access Database
+//docker-compose exec db psql -U postgres
 
 
 //ui
